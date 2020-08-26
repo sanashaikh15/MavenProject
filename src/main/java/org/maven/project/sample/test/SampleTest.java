29573473daec8997b4eb73220e8a0e8d087390c3
Sana Patel <ssohana.shaikh@gmail.com>
2020-08-26 00:00:09 -0500

    My First Commit

---
+++ src/main/java/org/maven/project/sample/test/SampleTest.java
@@ -0,0 +1,16 @@
+package org.maven.project.sample.test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+	}
+}
